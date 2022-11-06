39b0ddf90323f6f4e6dad283e2f1566d97d96836
joakimvu <joakim.vu@hiof.no>
2022-11-06 15:16:40 +0100

    Added store items

--- shopping-cart/src/pages/Store.tsx
+++ shopping-cart/src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import storeItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+
 export function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row lg={3} md={2} xs={1} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }

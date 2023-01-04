4df2396e1022c2d2cc3c4f9e7c985c0706260d96
Joakim Vu <joakimvu@icloud.com>
2023-01-04 21:33:49 +0100

    Update Card.js

--- frontend-lection-website/frontend/src/components/Card.js
+++ frontend-lection-website/frontend/src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card({ title, slug, description, category }) {
     // LÅNT KODE: Gjør første bokstav stor
     // https://flexiple.com/javascript-capitalize-first-letter/
     <article data-testid="course_wrapper">
-      <span>{category.charAt(0).toUpperCase() + category.slice(1)}</span>
+      <span>{category}</span>
       <h3 data-testid="courses_title">
         <a href={path}>{title}</a>
       </h3>
